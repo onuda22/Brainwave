31cf3976c3ded9f2f0957b5d87b8afe1f93848b1
= <=>
2025-01-16 22:09:39 +0700

    feat-benefits: implement card array of benefits

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import ButtonGradient from "./assets/svg/ButtonGradient";
+import Benefit from "./components/Benefit";
 import Button from "./components/Button";
 import Header from "./components/Header";
 import Hero from "./components/Hero";
@@ -9,6 +10,7 @@ const App = () => {
       <div className="pt-[4.75rem] lg:pt-[5.25rem]">
         <Header />
         <Hero />
+        <Benefit />
       </div>
       <ButtonGradient />
     </>
